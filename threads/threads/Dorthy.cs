b0774d89bf31e16f8939042eba478091667852be
Jaegar <jaegarls@icloud.com>
2024-03-05 08:24:17 -0330

    added threads assignment

---
+++ threads/threads/Dorthy.cs
@@ -0,0 +1,74 @@
+﻿namespace threads;
+
+public enum Color
+{
+    Silver, // Tin Man
+    Brown,  // Scarecrow
+    Yellow  // Cowardly Lion
+}
+// THREAD SAFE
+// public class Dorthy
+// {
+//     private string favoriteCharacter;
+//     private Color favoriteColor;
+//     private readonly object lockObject = new object();
+//
+//     public string FavoriteCharacter
+//     {
+//         get { lock (lockObject) return favoriteCharacter; }
+//         set 
+//         { 
+//             lock (lockObject)
+//             {
+//                 favoriteCharacter = value;
+//                 FavoriteCharacterChanged?.Invoke(favoriteCharacter);
+//             }
+//         }
+//     }
+//
+//     public Color FavoriteColor
+//     {
+//         get { lock (lockObject) return favoriteColor; }
+//         set 
+//         { 
+//             lock (lockObject)
+//             {
+//                 favoriteColor = value;
+//                 FavoriteColorChanged?.Invoke(favoriteColor.ToString());
+//             }
+//         }
+//     }
+//
+//     public event Action<string> FavoriteCharacterChanged;
+//     public event Action<string> FavoriteColorChanged;
+// }
+
+// NOT THREAD SAFE
+public class Dorthy
+{
+    private string favoriteCharacter;
+    private Color favoriteColor;
+
+    public string FavoriteCharacter
+    {
+        get { return favoriteCharacter; }
+        set 
+        { 
+            favoriteCharacter = value;
+            FavoriteCharacterChanged?.Invoke(favoriteCharacter);
+        }
+    }
+
+    public Color FavoriteColor
+    {
+        get { return favoriteColor; }
+        set 
+        { 
+            favoriteColor = value;
+            FavoriteColorChanged?.Invoke(favoriteColor.ToString());
+        }
+    }
+
+    public event Action<string> FavoriteCharacterChanged;
+    public event Action<string> FavoriteColorChanged;
+}
---
+++ threads/threads/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading; // For Thread
+using System.Collections.Generic; // For List
+
+namespace threads
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Dorthy dorthy = new Dorthy();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dorthy.FavoriteColorChanged += Dorthy_FavoriteColorChanged;
+            dorthy.FavoriteCharacterChanged += Dorthy_FavoriteCharacterChanged;
+            StartThreads();
+        }
+        private void StartThreads()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                new Thread(() => { dorthy.FavoriteCharacter = "Tin Man"; dorthy.FavoriteColor = Color.Silver; Thread.Sleep(1000);}).Start();
+                new Thread(() => { dorthy.FavoriteCharacter = "Scarecrow"; dorthy.FavoriteColor = Color.Brown;
+                    Thread.Sleep(1000);
+                }).Start();
+                new Thread(() => { dorthy.FavoriteCharacter = "Cowardly Lion"; dorthy.FavoriteColor = Color.Yellow;
+                    Thread.Sleep(1000);
+                }).Start();
+            }
+        }
+        
+        private void Dorthy_FavoriteColorChanged(string color)
+        {
+            Dispatcher.Invoke(() => favoriteColorTextBox.Text = color);
+        }
+
+        private void Dorthy_FavoriteCharacterChanged(string character)
+        {
+            Dispatcher.Invoke(() => favoriteCharacterTextBox.Text = character);
+        }
+        
+    }
+}
